c2538a95042ebf97411a6be0d1973bb150bfd158
rosekamallove <rosekamallove@gmail.com>
2021-07-15 22:21:15 +0530

    apiRequest to musixMatch

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Index from "./components/layout/Index";
+import { Provider } from "./context";
 import "./App.css";
 
 export default class App extends Component {
   render() {
     return (
-      <Router>
-        <React.Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Index} />
-            </Switch>
-          </div>
-        </React.Fragment>
-      </Router>
+      <Provider>
+        <Router>
+          <React.Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </Router>
+      </Provider>
     );
   }
 }
--- src/context.js
+++ src/context.js
@@ -1,6 +1,11 @@
+import axios from "axios";
 import React, { Component } from "react";
-
 const Context = React.createContext();
+
+const defaultURL = "https://api.musixmatch.com/ws/1.1/";
+const corsAnywhere = "https://cors-anywhere.herokuapp.com";
+const topChartURL =
+  "chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=";
 
 export class Provider extends Component {
   state = {
@@ -16,6 +21,21 @@ export class Provider extends Component {
     ],
     heading: "Top 10 Tracks",
   };
+
+  componentDidMount() {
+    axios
+      .get(
+        ` ${corsAnywhere}/${defaultURL}${topChartURL}${process.env.REACT_APP_MM_KEY}`
+      )
+      .then((res) => {
+        console.log(res);
+        this.setState({ track_list: res.data.message.body.track_list });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <Context.Provider value={this.state}>
