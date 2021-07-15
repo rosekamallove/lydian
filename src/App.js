10eb5d7b2f6fbe4938bde91b1b98f317a5bb8c1e
rosekamallove <rosekamallove@gmail.com>
2021-07-15 20:59:36 +0530

    component Structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import { Component } from "react";
-import "./App.css";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+import "./App.css";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-      </div>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
     );
   }
 }
