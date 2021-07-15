c7aa248f032dfbe8000f36eb4348e6fed2c0a2ac
rosekamallove <rosekamallove@gmail.com>
2021-07-16 00:38:32 +0530

    trackCard Created

--- src/components/tracks/Track.js
+++ src/components/tracks/Track.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Track = (props) => {
-  return <div>Track</div>;
+  const { track } = props;
+  return (
+    <div className="col-md-6">
+      <div className="card mb-4 shadow-sm">
+        <div className="card-body">
+          <h5>{track.artist_name}</h5>
+          <p className="card-text">
+            <strong>
+              <i className="fas fa-play"></i> Track:{" "}
+            </strong>
+            {track.track_name}
+            <br />
+            <strong>
+              <i className="fas fa-compact-disc"></i> Album:{" "}
+            </strong>
+            {track.album_name}
+          </p>
+        </div>
+        <Link
+          to={`lyrics/track/${track.track_id}`}
+          className="m-2 btn btn-dark btn-block"
+        >
+          <i className="fas fa-chevron-right"></i> View Lyrics
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Track;
